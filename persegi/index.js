2938e64be9debb2c98890e30cd94ec11b4d4f4b9
Frarif <freakinarif25@gmail.com>
2023-01-15 00:13:22 +0700

    Create: area count for persegi

---
+++ persegi/index.js
@@ -0,0 +1,14 @@
+const sideValue = parseInt( prompt ('Enter side value of rectangle: '))
+
+// calculate area of rectangle
+
+const roundValue = sideValue + sideValue + sideValue
+const areaValue = sideValue * sideValue
+
+console.log (
+    `The area of the rectangle is ${areaValue}`
+)
+
+console.log (
+    `The round of the rectangle is ${roundValue}`
+)
